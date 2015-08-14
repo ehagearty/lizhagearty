27861f7d19c757dc0ca9b0381b950eb7d0a05d0a
Elizabeth Hagearty <elizabeth@glassymedia.com>
2015-08-13 21:05:24 -0700

    initial commit of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lizhagearty::Application.config.secret_token = 'b5da84fb600b7015cd7268a38dd42e8c61902e9860feee64c3956b7222b39b04fb01847b44dbc2c4d2bc9bbd13d0dfdf3afb636ad838953b91d893b2b6db38ba'
